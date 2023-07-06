6f35645471a13edf4268d117c3dc143964f37850
amanapp <aman.dixit@appinventiv.com>
2023-07-06 20:44:03 +0530

    js core

---
+++ app/js/p1.js
@@ -0,0 +1,24 @@
+// sunny sky program 
+
+// by using let ,var & const 
+
+
+const someday="rainy_day";
+// someday="not_rainy_day";
+console.log(someday);
+
+your_choice=prompt("enter your choice betwen sun  , moon"," ");
+if(your_choice=="sun"){
+    console.log("day time");
+    let money=1;
+    var gift =2
+}else {
+    console.log("night time");
+    let money=2;
+    var gift=1;
+}
+// let is access in out of block 
+// console.log("money "+ money);
+
+// var is access out of block
+// console.log("gift "+gift);
---
+++ app/js/p2.js
@@ -0,0 +1,13 @@
+// while & do while
+
+let num=prompt("ENTER YOUR NUMBER between 0 to 3" ,0);
+while (num < 3) { 
+    alert( "loop while"+num );
+    num++;
+  }
+//op-  shows 0, then 1, then 2
+
+do{
+    alert("lop do while"+num);
+    num++;
+}while(num<3);
